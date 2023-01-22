01c15f37c442a207cfd53b3dfc21cd708e798830
Josh Musto <musto.josh@gmail.com>
2023-01-22 17:38:50 -0500

    pass02

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,1 +1,11 @@
 const Employee = require("../lib/Employee");
+
+describe("string describing the test suite", () => {
+    test("string describing a specific test", () => {
+        //the test
+        //declare some kind of input const
+
+        //declare the expected output from the input with an output const
+        //expect(function(input)).toEqual(output);
+    });
+});
