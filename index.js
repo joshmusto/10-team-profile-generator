c531a9bcfec1fc52a22aaee28f0b4fc7a780894f
Josh Musto <musto.josh@gmail.com>
2023-01-24 12:38:37 -0500

    pass05

--- index.js
+++ index.js
@@ -79,97 +79,102 @@ function writeEnd() { return `</section>
 const employeeArray = [];
 
 //use inquirer to get info
-inquirer
-  .prompt([
-    {
-      type: 'list',
-      message: 'What kind of employee would you like to add?',
-      name: 'role',
-      choices: ['Manager', 'Engineer', 'Intern'],
-    },
-    {
-        type: 'input',
-        message: `What is the employee's name?`,
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: `What is the employee's ID?`,
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: `What is the employee's email?`,
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: `What is the manager's office number?`,
-        name: 'officeNumber',
-        when: (answers) => answers.role === 'Manager',
-    },
-    {
-        type: 'input',
-        message: `What is the engineer's github username?`,
-        name: 'github',
-        when: (answers) => answers.role === 'Engineer',
-    },
-    {
-        type: 'input',
-        message: `What school is the intern from?`,
-        name: 'school',
-        when: (answers) => answers.role === 'Intern',
-    },
-    {
+function askQuestions() {
+    inquirer
+    .prompt([
+        {
         type: 'list',
-        message: 'Would you like to add another employee?',
-        name: 'again',
-        choices: ['Yes', "No"],
-      },
-  ])
-  .then(function(response) {
-    //set variables
-    let role = response.role;
-    let name = response.name;
-    let id = response.id;
-    let email = response.email;
-    if (role === "Manager") {
-        let officeNumber = response.officeNumber;
-        const addM = new Manager(name, id, email, officeNumber);
-        employeeArray.push(addM);
-    }
-    if (role === "Engineer") {
-        let github = response.github;
-        const addE = new Engineer(name, id, email, github);
-        employeeArray.push(addE);
-    }
-    if (role === "Intern") {
-        let school = response.school;
-        const addI = new Intern(name, id, email, school);
-        employeeArray.push(addI);
-    }
-    if (response.again === "Yes") {
-        //go again
-    }
-    else {
-        console.log(employeeArray);
-         //create and write file
-         //start
-         fs.writeFile("./dist/index.html", writeStart(), (err) =>
-         err ? console.log(err) : console.log('HTML writing has begun...'));
-         //employees
-         employeeArray.forEach(element => {
-            if (element==="Manager") {
-                fs.appendFile("./dist/index.html", writeManager(element), (err) => err ? console.log(err) : console.log(`Manager ${element.name} written...`)); 
-            }
-            else if (element==="Engineer") {
-                fs.appendFile("./dist/index.html", writeEngineer(element), (err) => err ? console.log(err) : console.log(`Engineer ${element.name} written...`)); 
-            }
-            else if (element==="intern") {
-                fs.appendFile("./dist/index.html", writeIntern(element), (err) => err ? console.log(err) : console.log(`Intern ${element.name} written...`)); 
-            }
-         })
-         //end
-         fs.appendFile("./dist/index.html", writeEnd(), (err) => err ? console.log(err) : console.log(`Generation complete! Enjoy your site!`));
-    }
-  });
+        message: 'What kind of employee would you like to add?',
+        name: 'role',
+        choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            message: `What is the employee's name?`,
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: `What is the employee's ID?`,
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: `What is the employee's email?`,
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: `What is the manager's office number?`,
+            name: 'officeNumber',
+            when: (answers) => answers.role === 'Manager',
+        },
+        {
+            type: 'input',
+            message: `What is the engineer's github username?`,
+            name: 'github',
+            when: (answers) => answers.role === 'Engineer',
+        },
+        {
+            type: 'input',
+            message: `What school is the intern from?`,
+            name: 'school',
+            when: (answers) => answers.role === 'Intern',
+        },
+        {
+            type: 'list',
+            message: 'Would you like to add another employee?',
+            name: 'again',
+            choices: ['Yes', "No"],
+        },
+    ])
+    .then(function(response) {
+        //set variables
+        let role = response.role;
+        let name = response.name;
+        let id = response.id;
+        let email = response.email;
+        if (role === "Manager") {
+            let officeNumber = response.officeNumber;
+            const addM = new Manager(name, id, email, officeNumber);
+            employeeArray.push(addM);
+        }
+        if (role === "Engineer") {
+            let github = response.github;
+            const addE = new Engineer(name, id, email, github);
+            employeeArray.push(addE);
+        }
+        if (role === "Intern") {
+            let school = response.school;
+            const addI = new Intern(name, id, email, school);
+            employeeArray.push(addI);
+        }
+        if (response.again === "Yes") {
+            askQuestions();
+        }
+        else {
+            console.log(employeeArray);
+            //create and write file
+            //start
+            fs.writeFile("./dist/index.html", writeStart(), (err) =>
+            err ? console.log(err) : console.log('HTML writing has begun...'));
+            
+            //employees
+            employeeArray.forEach(element => {
+                if (element==="Manager") {
+                    fs.appendFile("./dist/index.html", writeManager(element), (err) => err ? console.log(err) : console.log(`Manager ${element.name} written...`)); 
+                }
+                else if (element==="Engineer") {
+                    fs.appendFile("./dist/index.html", writeEngineer(element), (err) => err ? console.log(err) : console.log(`Engineer ${element.name} written...`)); 
+                }
+                else if (element==="intern") {
+                    fs.appendFile("./dist/index.html", writeIntern(element), (err) => err ? console.log(err) : console.log(`Intern ${element.name} written...`)); 
+                }
+            })
+            //end
+            fs.appendFile("./dist/index.html", writeEnd(), (err) => err ? console.log(err) : console.log(`Generation complete! Enjoy your site!`));
+        }
+    })
+};
+
+askQuestions();
